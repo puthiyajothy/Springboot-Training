3046e9675ffd3334f3b071311b61544d174bdf07
puthiyajothy <saidputhi@gmail.com>
2020-02-06 23:56:43 +0530

    sprinboot

---
+++ codelabes/src/main/java/com/java/training/model/Employee.java
@@ -0,0 +1,29 @@
+package com.java.training.model;
+
+public class Employee {
+	
+	String employeeName;
+	String employeeLocation;
+	
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getEmployeeLocation() {
+		return employeeLocation;
+	}
+	public void setEmployeeLocation(String employeeLocation) {
+		this.employeeLocation = employeeLocation;
+	}
+	
+	
+	public String getallemployee() {
+		 return this.employeeLocation="badulla";
+		
+	}
+	
+	
+
+}
