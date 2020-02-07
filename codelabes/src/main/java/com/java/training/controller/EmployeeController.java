98c2949bdf27528bc46aefa4ca44f69d68965f76
Mahendran Puthiyajothy <48283536+puthiyajothy@users.noreply.github.com>
2020-02-07 10:03:25 +0530

    Update EmployeeController.java

--- codelabes/src/main/java/com/java/training/controller/EmployeeController.java
+++ codelabes/src/main/java/com/java/training/controller/EmployeeController.java
@@ -1,30 +1,28 @@
 package com.java.training.controller;
 
 import java.util.List;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.java.training.model.Employee;
 
 
-@RestController("/service")
+@RestController
+@RequestMapping("/services")
 public class EmployeeController {
 
 	@RequestMapping("/hello")
 	public String GetallEmployee() {
 		return "<h1>hello springboot</h1>";
-		
-	}
-	
-	@RequestMapping("/getemployee")
-	public String getallemployee() {
-	    return "hellow";
-	  }
 
-	
- 
 	}
+
+	@RequestMapping("/employee")
+	public List<Employee> getEmployees() {
+		return Employee.getAllEmployees();
+
+	}
+
+}
 
 
 
