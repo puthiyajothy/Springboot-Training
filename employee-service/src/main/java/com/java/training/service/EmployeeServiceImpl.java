ee6be33ca27f983763024fbd9a467fd8ffdfc02b
Mahendran Puthiyajothy <48283536+puthiyajothy@users.noreply.github.com>
2020-02-12 16:51:22 +0530

    Update EmployeeServiceImpl.java

--- employee-service/src/main/java/com/java/training/service/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/java/training/service/EmployeeServiceImpl.java
@@ -7,12 +7,29 @@ import org.springframework.stereotype.Service;
 import com.java.training.model.Employee;
 import com.java.training.model.Telephone;
 import com.java.training.repository.EmployeeRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
+	
+	@Bean
+	RestTemplate getRestTemplate() {
+		
+		
+		return new RestTemplate();
+	}
+	
+	@Autowired
+	RestTemplate resttemplate;
 
 	@Override
 	public List<Employee> getallemployee() {
@@ -30,5 +47,29 @@ public class EmployeeServiceImpl implements EmployeeService {
 		return employeeRepository.save(employee);
 
 	}
+	
+	@Override
+	public Employee getemployeebyid(int id) {
+		
+		Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+		
+        if (optionalEmployee.isPresent()) {
+            Employee employee = optionalEmployee.get();
+            HttpHeaders httpheader = new HttpHeaders();
+            
+            HttpEntity<String> httpentity = new HttpEntity<String>("",httpheader);
+            ResponseEntity<Allocation[]> responseentity = resttemplate.exchange("http://localhost:8181/services/allocation/"+employee.getId(), HttpMethod.GET, httpentity, Allocation[].class);
+            employee.setAllocation(responseentity.getBody());
+           
+            return employee;
+       } 
+        
+       else {
+       
+        	return null;
+        	
+        }
+	}
+
 
 }
