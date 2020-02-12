7e97eb5071d2742412124570ab8d7811b2144a41
Mahendran Puthiyajothy <48283536+puthiyajothy@users.noreply.github.com>
2020-02-12 16:48:36 +0530

    Update AllocationServiceImpl.java

--- allocation-service/src/main/java/com/java/training/service/AllocationServiceImpl.java
+++ allocation-service/src/main/java/com/java/training/service/AllocationServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.java.training.model.Allocation;
 import com.java.training.repository.AllocationRepository;
+import org.springframework.data.domain.Example;
 
 @Service
 public class AllocationServiceImpl implements AllocationService {
@@ -22,5 +23,21 @@ public class AllocationServiceImpl implements AllocationService {
 	@Override
 	public List<Allocation> getallallocation() {
 		return allocationrepository.findAll();
+	}
+	
+	
+	@Override
+	public List<Allocation> getAllocationbyid(Integer id) {
+		//Optional<Allocation> optionalAllocation = allocationrepository.findById(id);
+		Allocation allocation = new Allocation();
+		allocation.setEmpId(id);
+		
+		Example<Allocation> allocationbyid = Example.of(allocation);
+		//if (optionalAllocation.isPresent()) {
+           // return optionalAllocation.get();
+        //}
+            
+            
+        return allocationrepository.findAll(allocationbyid);
 	}
 }
